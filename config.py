48556b085813f1898d37f744e1b8dd78345c542d
xwp <xwp_fullstack@163.com>
2018-06-02 10:11:04 +0800

    使用redis数据库保存session信息

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+from redis import StrictRedis
 class Config(object):
     DEBUG = True
-    # 配置密钥
+    # 配置密钥(注意密钥的设置方式)
 
     # 配置sqlalchemy，链接数据库
     # sqlalchemy是以url的形式来链接数据库的
@@ -8,5 +9,16 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/formal_database'
     # 配置数据库动态追踪修改（可以提前加载数据，跟踪数据库的变化）
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
+    # sqlalchemy只是存贮了模型类中的数据
+    # 还需要存贮session信息（用户的状态信息）,如果session没有保存到数据库，那么默认保存在浏览器中
+    # 使用redis保存session信息
+    # 配置redis基本信息
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = '6379'
+    SESSION_TYPE = 'redis'
+    # 对session信息进行签名编码
+    SESSION_USE_SIGNER = True
+    #
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 指定session信息过期时间,以秒为单位，设置是时间为一天
+    PERMANENT_SESSION_LIFETIME = 86400
--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, session
 from flask_script import Manager
 from config import Config
 from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
+# Session模块可以指定session信息的存贮
+from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
+# 实例化Session对象
+Session(app)
 db = SQLAlchemy(app)
 # 实例化管理器对象
 manager = Manager(app)
@@ -15,8 +19,13 @@ manager.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
+    # 能否实现下面session的存贮？
+    # 要设置密钥才能存贮
+    # 设置密钥后，启动服务器，将session信息存放在redis数据库中
+    # 在终端查看redis数据库
+    session['name'] = 2019
     return 'hello world'
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(port=5001)
 

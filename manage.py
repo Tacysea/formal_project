5b2bfceeb9bcab36892f7d00c8783acde49d1f52
xwp <xwp_fullstack@163.com>
2018-06-02 09:30:26 +0800

    集成script拓展包以及使用配置对象

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_script import Manager
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+# 实例化管理器对象
+manager = Manager(app)
 
 @app.route('/')
 def index():
     return 'hello world'
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    manager.run()
 
